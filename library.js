71fe308beeaa86aeaaa1d65135895fcb5e0b2ff1
Michael Juliano <mdj.michaeljuliano@gmail.com>
2025-06-17 20:40:23 -0400

    Create book object constructor and function to add new book to an array to store and test

---
+++ library.js
@@ -0,0 +1,22 @@
+//array to add books library to
+const myLibrary = [];
+
+//book object constructor
+function Book(title, author, pages) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+
+};
+
+//function to add new book object to array
+function addBookToLibrary(title, author, pages) {
+    let bookToAdd = new Book(title, author, pages)
+    myLibrary.push(bookToAdd);
+}
+
+addBookToLibrary("test", "author", "22");
+addBookToLibrary("test2", "author2", "222")
+console.log(myLibrary)
